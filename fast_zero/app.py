833e3841a157286912117476f559348fcbf275e5
Jeca <jecat4tu.games@gmail.com>
2025-08-16 21:10:56 -0300

    Implementação das Rotas CRUD

--- fast_zero/app.py
+++ fast_zero/app.py
@@ -1,8 +1,66 @@
-from fastapi import FastAPI
+from http import HTTPStatus
+
+from fastapi import FastAPI, HTTPException
+from fastapi.responses import HTMLResponse
+
+from fast_zero.schemas import Message, UserDB, UserList, UserPublic, UserSchema
 
 app = FastAPI()
 
+database = []
 
-@app.get('/')
+
+@app.get('/', status_code=HTTPStatus.OK, response_model=Message)
 def read_root():
     return {'message': 'Olá Mundo!'}
+
+
+@app.get('/helloworld', status_code=HTTPStatus.OK, response_class=HTMLResponse)
+def hello_world():
+    return """
+    <html>
+        <head>
+            <title>Olá Mundo!</title>
+        </head>
+        <body>
+            <h1>Olá mundo, bão?</h1>
+        </body>
+    </html>
+    """
+
+
+@app.post('/users/', status_code=HTTPStatus.CREATED, response_model=UserPublic)
+def create_user(user: UserSchema):
+    user_with_id = UserDB(**user.model_dump(), id=len(database) + 1)
+
+    database.append(user_with_id)
+
+    return user_with_id
+
+
+@app.get('/users/', status_code=HTTPStatus.OK, response_model=UserList)
+def list_users():
+    return {'users': database}
+
+
+@app.put('/users/{user_id}', response_model=UserPublic)
+def update_user(user_id: int, user: UserSchema):
+    if user_id > len(database) or user_id < 1:
+        raise HTTPException(
+            status_code=HTTPStatus.NOT_FOUND, detail='User not found'
+        )
+    user_with_id = UserDB(**user.model_dump(), id=user_id)
+    database[user_id - 1] = user_with_id
+
+    return user_with_id
+
+
+@app.delete('/users/{user_id}', response_model=Message)
+def delete_user(user_id: int):
+    if user_id > len(database) or user_id < 1:
+        raise HTTPException(
+            status_code=HTTPStatus.NOT_FOUND, detail='User not found'
+        )
+    del database[user_id - 1]
+
+    return {'message': 'User deleted'}
--- tests/test_app.py
+++ tests/test_app.py
@@ -1,15 +1,67 @@
 from http import HTTPStatus
 
-from fastapi.testclient import TestClient
 
-from fast_zero.app import app
+def test_root_deve_retornar_ok_e_ola_mundo(client):
+    response = client.get('/')  # Act
+
+    assert response.status_code == HTTPStatus.OK  # Assert
+    assert response.json() == {'message': 'Olá Mundo!'}  # Assert
 
 
-def test_root_deve_retornar_ok_e_ola_mundo():
+def teste_html_deve_retornar_ok_e_olamundo(client):
+    response = client.get('/helloworld')
 
-    client = TestClient(app) # Arrange
+    assert response.status_code == HTTPStatus.OK
+    assert '<h1>Olá mundo, bão?</h1>' in response.text
 
-    response = client.get('/') # Act
 
-    assert response.status_code == HTTPStatus.OK # Assert
-    assert response.json() == {'message': 'Olá Mundo!'} # Assert
+def teste_post_user(client):
+    response = client.post(
+        '/users/',
+        json={
+            'username': 'joao',
+            'email': 'joar@email.com',
+            'password': 'segredo',
+        },
+    )
+
+    assert response.status_code == HTTPStatus.CREATED
+    assert response.json() == {
+        'username': 'joao',
+        'email': 'joar@email.com',
+        'id': 1,
+    }
+
+
+def teste_get_users(client):
+    response = client.get('/users/')
+
+    assert response.status_code == HTTPStatus.OK
+    assert response.json() == {
+        'users': [{'username': 'joao', 'email': 'joar@email.com', 'id': 1}]
+    }
+
+
+def teste_update_user(client):
+    response = client.put(
+        '/users/1',
+        json={
+            'username': 'carlos',
+            'email': 'carlos@email.com',
+            'password': 'senhasegura',
+        },
+    )
+
+    assert response.status_code == HTTPStatus.OK
+    assert response.json() == {
+        'username': 'carlos',
+        'email': 'carlos@email.com',
+        'id': 1,
+    }
+
+
+def teste_delete_user(client):
+    response = client.delete('/users/1')
+
+    assert response.status_code == HTTPStatus.OK
+    assert response.json() == {'message': 'User deleted'}
